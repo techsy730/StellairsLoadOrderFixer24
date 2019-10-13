12c8d6dac9f75498a5755b4411dcf0a60824a6e4
C. Sean Young <techsy730@gmail.com>
2019-10-13 15:38:12 -0500

    Update to only enabled mods that are already enabled.
    Also fixes a call to ctypes.windll even when not on a Windows system.

--- load_order_stellaris24.py
+++ load_order_stellaris24.py
@@ -30,13 +30,15 @@ def getModList(data):
 	return modList
 
 
-def writeLoadOrder(idList, dlc_load):
+def writeLoadOrder(idList, dlc_load, enabled_mods):
     data = {}
     with open(dlc_load, 'r+') as json_file:
         data = json.load(json_file)
 
     if len(data) < 1:
         abort('dlc_load.json loading failed')
+    if enabled_mods is not None:
+    	idList = [m for m in idList if m in enabled_mods]
     data['enabled_mods'] = idList
 
     with open(dlc_load, 'w') as json_file:
@@ -65,13 +67,22 @@ def run(settingPath):
         modList = getModList(data)
     if len(modList) <= 0:
         abort('no mod found')
+    enabled_mods = None
+    if os.path.exists(dlc_load):
+	    with open(dlc_load) as dlc_load_file:
+	    	dlc_load_data = json.load(dlc_load_file)
+	    	# Do some legwork ahead of time to put into a set to avoidic quadratic loop later for filtering.
+	    	enabled_mods = frozenset(dlc_load_data.get("enabled_mods", []))
     idList = [mod.modId for mod in modList]
     hashList = [mod.hashKey for mod in modList]
     writeDisplayOrder(hashList, game_data)
-    writeLoadOrder(idList, dlc_load)
+    writeLoadOrder(idList, dlc_load, enabled_mods)
 
 def Mbox(title, text, style):
-    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+		if hasattr(ctypes, "windll"):
+	    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+    else:
+    	print(text)
 
 # check Stellaris settings location
 locations = [os.path.join(os.path.expanduser('~'), 'Documents', 'Paradox Interactive', 'Stellaris'), ".", "..", os.path.join(os.path.expanduser('~'), '.local', 'share','Paradox Interactive', 'Stellaris')]
