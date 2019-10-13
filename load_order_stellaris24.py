58233eff9d9a1dfe3834b0875352b6716d1d6fe5
C. Sean Young <techsy730@gmail.com>
2019-10-13 15:43:13 -0500

    Be consistent with spaces instead of tabs.

--- load_order_stellaris24.py
+++ load_order_stellaris24.py
@@ -38,7 +38,7 @@ def writeLoadOrder(idList, dlc_load, enabled_mods):
     if len(data) < 1:
         abort('dlc_load.json loading failed')
     if enabled_mods is not None:
-    	idList = [m for m in idList if m in enabled_mods]
+        idList = [m for m in idList if m in enabled_mods]
     data['enabled_mods'] = idList
 
     with open(dlc_load, 'w') as json_file:
@@ -69,20 +69,20 @@ def run(settingPath):
         abort('no mod found')
     enabled_mods = None
     if os.path.exists(dlc_load):
-	    with open(dlc_load) as dlc_load_file:
-	    	dlc_load_data = json.load(dlc_load_file)
-	    	# Do some legwork ahead of time to put into a set to avoidic quadratic loop later for filtering.
-	    	enabled_mods = frozenset(dlc_load_data.get("enabled_mods", []))
+        with open(dlc_load) as dlc_load_file:
+            dlc_load_data = json.load(dlc_load_file)
+            # Do some legwork ahead of time to put into a set to avoidic quadratic loop later for filtering.
+            enabled_mods = frozenset(dlc_load_data.get("enabled_mods", []))
     idList = [mod.modId for mod in modList]
     hashList = [mod.hashKey for mod in modList]
     writeDisplayOrder(hashList, game_data)
     writeLoadOrder(idList, dlc_load, enabled_mods)
 
 def Mbox(title, text, style):
-		if hasattr(ctypes, "windll"):
-	    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+    if hasattr(ctypes, "windll"):
+        return ctypes.windll.user32.MessageBoxW(0, text, title, style)
     else:
-    	print(text)
+        print(text)
 
 # check Stellaris settings location
 locations = [os.path.join(os.path.expanduser('~'), 'Documents', 'Paradox Interactive', 'Stellaris'), ".", "..", os.path.join(os.path.expanduser('~'), '.local', 'share','Paradox Interactive', 'Stellaris')]
